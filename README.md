df091889d0bb52bd133a56bd4b02346378ce5dd3
brian munoz <brianmunozdev@gmail.com>
2019-09-01 15:57:24 -0700

    add multiples of 3 and 5 solution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # project-euler
 a series of challenging mathematical/computer programming problems
+
+### list of problems
+
+[multiples of 3 and 5](https://projecteuler.net/problem=1)
---
+++ multiples-of-3-and-5.js
@@ -0,0 +1,7 @@
+// if you read this you are really cool :)
+
+for (let i = 1; i <= 1000; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    console.log(`${i} is a mutiple of 3 or 5`);
+  }
+}
